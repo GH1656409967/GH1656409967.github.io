edc146e28df937efb3e13b6d73dfb0a5f8d2c3d2
GH1656409967 <1656409967@qq.com>
2022-04-26 16:28:57 +0800

    Site updated: 2022-04-26 16:28:54

--- js/fold_action.js
+++ js/fold_action.js
@@ -1,8 +1,1 @@
-$(document).ready(function () {
-    $(document).on('click', '.fold_hider', function () {
-        $('>.fold', this.parentNode).slideToggle();
-        $('>:first', this).toggleClass('open');
-    });
-    //默认情况下折叠
-    $("div.fold").css("display", "none");
-});
+$(document).ready((function(){$(document).on("click",".fold_hider",(function(){$(">.fold",this.parentNode).slideToggle(),$(">:first",this).toggleClass("open")})),$("div.fold").css("display","none")}));
