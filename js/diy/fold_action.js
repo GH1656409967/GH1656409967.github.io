0a342137d2bd3cf6db00d4cd2beeadc9b1cd703e
GH1656409967 <1656409967@qq.com>
2023-07-04 19:59:15 +0800

    Site updated: 2023-07-04 19:59:15

--- js/diy/fold_action.js
+++
@@ -1,1 +0,0 @@
-$(document).ready((function(){$(document).on("click",".fold_hider",(function(){$(">.fold",this.parentNode).slideToggle(),$(">:first",this).toggleClass("open")})),$("div.fold").css("display","none")}));
